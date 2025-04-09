243b81531ddc8c8a99b42a920bd438db93851daa
Neil Jason Cañete <96198550+NxJxCx@users.noreply.github.com>
2025-04-09 20:50:22 +0800

    ok

--- server.py
+++ server.py
@@ -15,6 +15,7 @@ cors_origins = [
     "https://localhost:5000",
     "http://127.0.0.1:5000",
     "https://127.0.0.1:5000",
+    "https://capstone.smccnasipit.edu.ph/befs",
 ]
 
 @asynccontextmanager
